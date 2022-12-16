c289f5eb74f0c110262abf2f2c7a43d23757d9ce
lby132 <quddbscjswow@naver.com>
2022-12-16 10:50:39 +0900

    어노테이션 프로세서 만들기

---
+++ src/main/java/me/whiteship/MagicMojaProcessor.java
@@ -0,0 +1,45 @@
+package me.whiteship;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class MagicMojaProcessor extends AbstractProcessor {
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Set.of(Magic.class.getName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        final Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Magic.class);
+        for (Element element : elements) {
+            final Name elementName = element.getSimpleName();
+            if (element.getKind() != ElementKind.INTERFACE) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Magic annotation can not");
+            } else {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Processing " + elementName);
+            }
+        }
+
+        return true;
+    }
+}
